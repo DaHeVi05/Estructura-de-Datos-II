b18451f1fc24cc9ae5a4e106e1aa3558b912917a
DaHeVi05 <david.hernandezvidriales@cesunbc.edu.mx>
2024-09-10 22:59:44 -0700

    Primer Trabajo

---
+++ Hola.java
@@ -0,0 +1,21 @@
+public class Hola {
+    
+
+        public static void main(String[] args) {
+        int x = 5;
+        String y = "John";
+
+            System.out.println(y);
+            System.out.println(x);
+            System.out.println("Hola Mundo");
+
+        int n = 0;
+        while (n < 10) {
+            n = n + 1;
+            System.out.println("Hola Maestra");
+            n++;
+        }
+
+        }
+
+}
